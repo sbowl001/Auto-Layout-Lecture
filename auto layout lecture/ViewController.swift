b3e94360448b38ee16f2b45edc00792666929d16
Stephanie Bowles <sbowl001@fiu.edu>
2019-05-02 10:00:46 +0800

    init

---
+++ auto layout lecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  auto layout lecture
+//
+//  Created by Stephanie Bowles on 5/2/19.
+//  Copyright © 2019 Stephanie Bowles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
